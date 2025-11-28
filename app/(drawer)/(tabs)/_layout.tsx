27080b38c86219547297a46bd3cf1eab9acb023b
350ops <350ops@users.noreply.github.com>
2025-11-28 04:05:36 +0300

    refactor: Migrate tab navigation to `unstable-native-tabs` and configure EAS builds with New Architecture disabled.

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,60 +1,32 @@
-import { useThemeColors } from 'app/contexts/ThemeColors';
-import { TabButton } from 'components/TabButton';
-import { Tabs, TabList, TabTrigger, TabSlot } from 'expo-router/ui';
-import { View } from 'react-native';
+import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 import React from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import ActionTab from '@/components/ActionTab';
 
 export default function Layout() {
-  const colors = useThemeColors();
-  const insets = useSafeAreaInsets();
   return (
-    <Tabs>
-      <TabSlot />
-      <TabList
-        style={{
-          alignItems: 'center',
-          backgroundColor: colors.secondary,
-          paddingBottom: insets.bottom,
-        }}
-      >
-        {/* Home Tab */}
-        <TabTrigger
-          name="index"
-          href="/"
-          asChild
-        >
-          <TabButton labelAnimated={true} icon="Home">Home</TabButton>
-        </TabTrigger>
+    <NativeTabs
+      blurEffect="systemChromeMaterialDark"
+      backgroundColor="transparent"
+      shadowColor="transparent"
+    >
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" />
+      </NativeTabs.Trigger>
 
-        <TabTrigger
-          name="cards"
-          href="/cards"
-          asChild
-        >
-          <TabButton labelAnimated={true} icon="CreditCard">Cards</TabButton>
-        </TabTrigger>
+      <NativeTabs.Trigger name="cards">
+        <Label>Cards</Label>
+        <Icon sf="creditcard.fill" />
+      </NativeTabs.Trigger>
 
-      
-        <TabTrigger
-          name="recipients"
-          href="/recipients"
-          asChild
-        >
-          <TabButton labelAnimated={true} icon="Users">Recipients</TabButton>
-        </TabTrigger>
+      <NativeTabs.Trigger name="recipients">
+        <Label>Recipients</Label>
+        <Icon sf="person.2.fill" />
+      </NativeTabs.Trigger>
 
-        <TabTrigger
-          name="payments"
-          href="/payments"
-          asChild
-        >
-          <TabButton labelAnimated={true} icon="ArrowDownUp">Payments</TabButton>
-        </TabTrigger>
-
-
-      </TabList>
-    </Tabs>
+      <NativeTabs.Trigger name="payments">
+        <Label>Payments</Label>
+        <Icon sf="arrow.up.arrow.down" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
